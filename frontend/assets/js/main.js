9df6736a72389667f7334d38e06ab48cbf113991
Benedek Voros <xbenedek01@gmail.com>
2023-03-30 04:35:16 +0100

    something that works

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,17 +1,24 @@
 function getNextQuestion() {
     var questionContainer = document.getElementById("question-container");
-    var id = questionContainer.childElementCount + 1;
-  
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var question = JSON.parse(this.responseText);
-        var questionDiv = document.createElement("div");
-        questionDiv.innerHTML = "<p>" + question.question + "</p>";
-        questionContainer.appendChild(questionDiv);
-      }
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                var question = JSON.parse(this.responseText);
+                var questionText = document.createTextNode(question.question);
+                questionContainer.innerHTML = '';
+                questionContainer.appendChild(questionText);
+            } else if (this.status == 500) {
+                questionContainer.innerHTML = "Nincs tobb kerdes!";
+            }
+        }
     };
-    xhttp.open("GET", "/question?id=" + id, true);
-    xhttp.send();
-  }
-  
+    request.onerror = function() {
+        questionContainer.innerHTML = "Nincs tobb kerdes!";
+    };
+    request.open("GET", "/question?id=" + getNextQuestion.questionIndex, true);
+    request.send();
+    getNextQuestion.questionIndex++;
+}
+
+getNextQuestion.questionIndex = 1;
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -13,6 +13,7 @@ func NewRouter(storage *storage.Storage) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.IndexHandler)
 	mux.HandleFunc("/question", questionHandler.GetQuestionByID)
-
+	mux.Handle("/assets/css/", http.StripPrefix("/assets/css/", http.FileServer(http.Dir("frontend/assets/css/"))))
+	mux.Handle("/assets/js/", http.StripPrefix("/assets/js/", http.FileServer(http.Dir("frontend/assets/js/"))))
 	return mux
 }
