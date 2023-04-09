b1e8b3d84af6f9b8d93b5570839b51ae95cc58d1
Benedek Voros <xbenedek01@gmail.com>
2023-04-09 05:20:04 +0100

    bootstrap

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,7 +1,5 @@
 class App {
     constructor() {
-      this.correctAnswer = '';
-  
       this.bindEvents();
       this.getRandomQuestion();
     }
@@ -29,20 +27,18 @@ class App {
         const questionHTML = data.question;
         questionDiv.innerHTML = questionHTML;
   
-        const table = this.createAnswerTable(data);
+        const answerContainer = this.createAnswerContainer(data);
         const questionAnswer = document.getElementById('questionAnswer');
         questionAnswer.innerHTML = "";
-        questionAnswer.appendChild(table);
+        questionAnswer.appendChild(answerContainer);
   
         await MathJax.typesetPromise();
   
         const img = document.getElementById('questionImage');
         img.src = data.imageLink;
-    
+  
         this.questionID = data.id; // Store the question ID
         this.answerHolders = data.answerHolders; // Store the answer holders
-        
-        this.correctAnswer = data.answer;
   
         // Set question info
         const questionInfo = document.getElementById("questionInfo");
@@ -52,82 +48,95 @@ class App {
           const semesterText = data.semester === 1 ? 'tavaszi' : 'őszi';
           questionInfo.innerText = `Ez volt a ${data.number}. feladat a ${data.year}-s ${semesterText} érettségiben.`;
         }
-
+  
       } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
       }
     }
   
     async checkAnswer() {
-        const answerInputs = document.getElementsByClassName("answerInput");
-        const postData = [];
-    
-        for (let i = 0; i < answerInputs.length; i++) {
-          postData.push({
-            answer_holder_id: this.answerHolders[i].id,
-            answer: answerInputs[i].value,
-          });
-        }
-    
-        try {
-          const response = await fetch(`/question/${this.questionID}/check_answers`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(postData),
-          });
-    
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-    
-          const data = await response.json();
-    
-          for (let i = 0; i < data.length; i++) {
-            const answerResult = document.getElementsByClassName("answerResult")[i];
-            answerResult.textContent = data[i].answers.join(" vagy ");
-            answerResult.style.backgroundColor = data[i].correct ? 'limegreen' : 'red';
-          }
-        } catch (error) {
-          console.error('There has been a problem with your fetch operation:', error);
-        }
+      const answerInputs = document.getElementsByClassName("answerInput");
+      const postData = [];
+  
+      for (let i = 0; i < answerInputs.length; i++) {
+        postData.push({
+          answer_holder_id: this.answerHolders[i].id,
+          answer: answerInputs[i].value,
+        });
       }
   
-    createAnswerTable(data) {
+      try {
+        const response = await fetch(`/question/${this.questionID}/check_answers`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(postData),
+        });
+  
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+  
+        const data = await response.json();
+  
+        for (let i = 0; i < data.length; i++) {
+          const answerResult = document.getElementsByClassName("answerResult")[i];
+          answerResult.textContent = data[i].answers.join(" vagy ");
+          answerResult.style.backgroundColor = data[i].correct ? 'limegreen' : 'red';
+        }
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+      }
+    }
+  
+    createAnswerContainer(data) {
       const answerHolders = data.answerHolders;
-      const table = document.createElement('table');
-      for (let i = 0; i < answerHolders.length; i++) {  
-        const tr = document.createElement('tr');
-        const td1 = document.createElement('td');
-        td1.className = 'answerPrefix';
+      const container = document.createElement('div');
+      container.className = 'container';
+  
+      for (let i = 0; i < answerHolders.length; i++) {
+        const row = document.createElement('div');
+        row.className = 'row';
+  
+        const col1 = document.createElement('div');
+        col1.className = 'col-md-3';
         const p1 = document.createElement('p');
         p1.textContent = answerHolders[i].prefix;
-        td1.appendChild(p1);
-        tr.appendChild(td1);
-        const td2 = document.createElement('td');
-        td2.className = 'answerText';
+        col1.appendChild(p1);
+        row.appendChild(col1);
+
+        const col2 = document.createElement('div');
+        col2.className = 'col-md-3';
         const input = document.createElement('input');
         input.placeholder = 'válasz';
         input.className = 'answerInput';
-        td2.appendChild(input);
-        tr.appendChild(td2);
-        const td3 = document.createElement('td');
-        td3.className = 'answerSuffix';
+        col2.appendChild(input);
+        row.appendChild(col2);
+  
+        const col3 = document.createElement('div');
+        col3.className = 'col-md-3';
         const p2 = document.createElement('p');
         p2.textContent = answerHolders[i].suffix;
-        td3.appendChild(p2);
-        tr.appendChild(td3);
-        const td4 = document.createElement('td');
-        td4.className = 'answerResult';
-        tr.appendChild(td4);
-        table.appendChild(tr);
+        col3.appendChild(p2);
+        row.appendChild(col3);
+  
+        const col4 = document.createElement('div');
+        col4.className = 'col-md-3';
+        const answerResult = document.createElement('p');
+        answerResult.className = 'answerResult';
+        col4.appendChild(answerResult);
+        row.appendChild(col4);
+  
+        container.appendChild(row);
       }
-      return table;
+      return container;
     }
-  };
+  }
   
   window.addEventListener("DOMContentLoaded", () => {
     const app = new App();
   });
-    
+  
+       
+  
