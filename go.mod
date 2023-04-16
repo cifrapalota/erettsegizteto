977af1582fdb59d600b96a0a5627c11b35233774
Benedek Voros <xbenedek01@gmail.com>
2023-04-16 08:27:25 +0100

    test newrelic

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/newrelic/go-agent/v3 v3.21.0
 	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11
 )
 
@@ -15,6 +16,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.0 // indirect
@@ -32,6 +34,8 @@ require (
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.49.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,14 +2,32 @@ package routers
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	"hu.erettsegizteto/internal/handlers"
 )
 
 // NewRouter creates a new http handler
 func NewRouter(handler *handlers.Handler) http.Handler {
 	router := gin.Default()
+
+	license := os.Getenv("NEW_RELIC_LICENSE_KEY")
+	if license == "" {
+		return nil
+	}
+
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("erettsegizteto"),
+		newrelic.ConfigLicense(license),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Add the New Relic middleware
+	router.Use(NewRelicMiddleware(app))
 
 	// Add the logging middleware
 	router.Use(handlers.LoggingMiddleware(handler.Logger))
@@ -25,4 +43,19 @@ func NewRouter(handler *handlers.Handler) http.Handler {
 	router.Static("/assets/img", "frontend/assets/img")
 
 	return router
+}
+
+func NewRelicMiddleware(app *newrelic.Application) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		txn := app.StartTransaction(c.Request.URL.Path)
+		defer txn.End()
+
+		txn.SetWebRequestHTTP(c.Request)
+		c.Request = newrelic.RequestWithTransactionContext(c.Request, txn)
+
+		c.Next()
+
+		txn.SetWebResponse(c.Writer)
+		txn.SetName(c.HandlerName())
+	}
 }
